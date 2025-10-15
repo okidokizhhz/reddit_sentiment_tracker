b8c492a61724e96a5b44cdab1618ebbc00b5c5d4
okidoki <akm1000@hotmail.de>
2025-10-15 16:52:22 +0200

    fixed comments and variable names

--- src/main.py
+++ src/main.py
@@ -4,61 +4,106 @@ import sys
 from .data_collection.reddit_client import get_reddit_client
 from .data_collection.post_fetcher import fetch_top_posts, fetch_rising_posts
 from .data_collection.comment_fetcher import fetch_comments
+from .data_collection.subreddit_fetcher import fetch_subreddit_metadata
 from .config import RATE_LIMIT_RISING_POSTS, RATE_LIMIT_TOP_POSTS, COMMENT_LIMIT, TOP_POSTS_TIME_FILTER, REPLY_DEPTH
 from .storage.connection import initialize_database
+from .storage.crud import insert_subreddit_metadata, insert_top_posts, insert_rising_posts
 from .logger import setup_logger
 
 logger = setup_logger("reddit_sentiment_tracker")
 
 def main():
+    subreddit_name = "wien"
+
+    # Database Initialization
     try:
         initialize_database()
         logger.info("Database initialized successfully")
     except Exception as e:
-        logger.critical(f"Failed to initialize DB: {e}", exc_info=True)
+        logger.critical(f"Failed to initialize DB: {e}. Exiting Program", exc_info=True)
         sys.exit(1)
 
-    try:    # get reddit Client
+    # Reddit Client
+    try:
         reddit = get_reddit_client()
+        logger.info("Reddit Client retrieved successfully")
     except Exception as e:
-        logger.fatal(f"Failed to retrieve Reddit client. Exiting Program: {e}", exc_info=True)
+        logger.fatal(f"Failed to retrieve Reddit client: {e}. Exiting Program", exc_info=True)
         sys.exit(1)         # Exit program on failure
 
-    # get Top Posts
+
+    # Subreddit Metadata fetching
     try:
-        top_posts_data = fetch_top_posts("wien",
+        subreddit_metadata = fetch_subreddit_metadata(subreddit_name, reddit)
+        logger.info(f"Subreddit metadata of 'r/{subreddit_name}' fetched successfully")
+        subreddit_id = subreddit_metadata["id"]
+    except Exception as e:
+        logger.error(f"Failed to fetch metadata of: r/{subreddit_name}", exc_info=True)
+
+
+    # DB: inserting Metadata
+    try:
+        insert_subreddit_metadata(subreddit_metadata)
+        logger.info(f"Subreddit metadata of 'r/{subreddit_name}' inserted into DB successfully")
+    except Exception as e:
+        logger.error(f"Failed to insert subreddit metadata of 'r/{subreddit_name}' into DB: {e}", exc_info=True)
+
+
+    # Top Posts fetching
+    try:
+        top_posts_data = fetch_top_posts(subreddit_name,
                                          reddit,
                                          RATE_LIMIT_TOP_POSTS,
                                          TOP_POSTS_TIME_FILTER)
-        # get comments of Top Posts
-        try:
-            for post in top_posts_data:
-                post_id = post["id"]
-                post["comments"] = fetch_comments(reddit,
-                                                  post_id,
-                                                  REPLY_DEPTH,
-                                                  COMMENT_LIMIT)
-        except Exception as e:
-            logger.error(f"Failed to fetch comments for Top Posts", exc_info=True)
     except Exception as e:
         logger.error(f"Failed to fetch top posts", exc_info=True)
 
-    # get Rising Posts
+    # DB: inserting Top Posts
     try:
-        rising_posts_data = fetch_rising_posts("wien",
+        insert_top_posts(top_posts_data, subreddit_id)
+        logger.info("Inserting top posts data into DB successful")
+    except Exception as e:
+        logger.error(f"Failed to insert top posts data into DB: {e}", exc_info=True)
+
+    # Top Posts Comments fetching
+    try:
+        for post in top_posts_data:
+            post_id = post["id"]
+            post["comments"] = fetch_comments(reddit,
+                                              post_id,
+                                              REPLY_DEPTH,
+                                              COMMENT_LIMIT)
+    except Exception as e:
+        logger.error(f"Failed to fetch comments for Top Posts", exc_info=True)
+
+
+    # Rising Posts fetching
+    try:
+        rising_posts_data = fetch_rising_posts(subreddit_name,
                                                reddit,
                                                RATE_LIMIT_RISING_POSTS)
-        # get comments of Rising Posts
-        try:
-            for post in rising_posts_data:
-                post["comments"] = fetch_comments(reddit,
-                                                  post["id"],
-                                                  REPLY_DEPTH,
-                                                  COMMENT_LIMIT)
-        except Exception as e:
-            logger.error(f"Failed to fetch comments for Rising Posts", exc_info=True)
     except Exception as e:
-        logger.error(f"failed to fetch rising posts", exc_info=True)
+        logger.error(f"failed to fetch rising posts: {e}", exc_info=True)
+
+
+    # DB: inserting Rising Posts
+    try: 
+        insert_rising_posts(rising_posts_data, subreddit_id)
+        logger.info("Inserting rising posts data into DB successful")
+    except Exception as e:
+        logger.error(f"Failed to insert rising posts data into DB: {e}", exc_info=True)
+
+
+    # Rising Posts Comments fetching
+    try:
+        for post in rising_posts_data:
+            post["comments"] = fetch_comments(reddit,
+                                              post["id"],
+                                              REPLY_DEPTH,
+                                              COMMENT_LIMIT)
+    except Exception as e:
+        logger.error(f"Failed to fetch comments for Rising Posts", exc_info=True)
+
 
 if __name__ == "__main__":
     main()
--- src/storage/crud.py
+++ src/storage/crud.py
@@ -20,6 +20,10 @@ def db_session():
 
 def insert_subreddit_metadata(subreddit_metadata: dict):
     """ Inserting subreddit metadata into DB in a transaction """
+    if not subreddit_metadata:
+        logger.info("No subreddit metadata to insert")
+        return
+
     try:
         with db_session() as conn:
             conn.execute(subreddits.insert(), subreddit_metadata)
@@ -30,20 +34,52 @@ def insert_subreddit_metadata(subreddit_metadata: dict):
         logger.error(f"Failure inserting subreddit metadata into DB: {e}", exc_info=True)
         raise
 
-def insert_top_posts(posts_data, subreddit_id):
+def insert_top_posts(top_posts_data, subreddit_id):
     """ Inserting posts data into DB in a transaction """
-    if not posts_data:
+    if not top_posts_data:
         logger.info("No posts data to insert")
         return
 
     posts_to_insert = []
 
-    for post in posts_data:
+    for post in top_posts_data:
         post_db_data = {
             "id": post["id"],
             "subreddit_id": subreddit_id,
             "author": post["author"],
             "post_type": "top",
+            "title": post["title"],
+            "selftext": post["selftext"],
+            "url": post["url"],
+            "flair": post["flair"],
+            "created_utc": post["created_utc"]
+        }
+        posts_to_insert.append(post_db_data)
+
+    try:
+        with db_session() as conn:
+            conn.execute(posts.insert(), posts_to_insert)
+
+        logger.info(f"Successfully inserted posts data into DB")
+
+    except Exception as e:
+        logger.error(f"Failure inserting posts data into DB: {e}", exc_info=True)
+        raise
+
+def insert_rising_posts(rising_posts_data, subreddit_id):
+    """ Inserting rising posts data into DB in a transaction """
+    if not rising_posts_data:
+        logger.info("No posts data to insert")
+        return
+
+    posts_to_insert = []
+
+    for post in rising_posts_data:
+        post_db_data = {
+            "id": post["id"],
+            "subreddit_id": subreddit_id,
+            "author": post["author"],
+            "post_type": "rising",
             "title": post["title"],
             "selftext": post["selftext"],
             "url": post["url"],
