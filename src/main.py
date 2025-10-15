242a24c18656a16cdea69570104b7996a101a46d
okidoki <akm1000@hotmail.de>
2025-10-15 13:05:21 +0200

    implemented Database Initialization in main()

--- src/main.py
+++ src/main.py
@@ -5,13 +5,20 @@ from .data_collection.reddit_client import get_reddit_client
 from .data_collection.post_fetcher import fetch_top_posts, fetch_rising_posts
 from .data_collection.comment_fetcher import fetch_comments
 from .config import RATE_LIMIT_RISING_POSTS, RATE_LIMIT_TOP_POSTS, COMMENT_LIMIT, TOP_POSTS_TIME_FILTER, REPLY_DEPTH
+from .storage.connection import initialize_database
 from .logger import setup_logger
 
 logger = setup_logger("reddit_sentiment_tracker")
 
 def main():
-    # get Reddit Client
     try:
+        initialize_database()
+        logger.info("Database initialized successfully")
+    except Exception as e:
+        logger.critical(f"Failed to initialize DB: {e}", exc_info=True)
+        sys.exit(1)
+
+    try:    # get reddit Client
         reddit = get_reddit_client()
     except Exception as e:
         logger.fatal(f"Failed to retrieve Reddit client. Exiting Program: {e}", exc_info=True)
