01c070a571bdd9d7e3c66be1d4eb858d724358b6
okidoki <akm1000@hotmail.de>
2025-10-22 16:42:12 +0200

    added /register /login endpoints with JWT authentication

--- server.py
+++ server.py
@@ -3,13 +3,14 @@
 import sys
 from datetime import datetime, timezone
 from contextlib import asynccontextmanager
-from typing import Optional, Any, Dict, List
-
-from fastapi import FastAPI
-
-from src.logger import setup_logger
+from typing import Any, Dict, List
+from fastapi import FastAPI, HTTPException
+from src.storage.schema_manager import users
+from src.api.models import (RegisterRequest, RegisterResponse, LoginRequest, LoginResponse)
+from src.api.bcrypt_hashing import hash_password, verify_password
 from src.storage.connection import initialize_database
-from src.storage.crud import retrieve_metadata, retrieve_posts_data, retrieve_comments_data
+from src.storage.crud import retrieve_metadata, retrieve_posts_data, retrieve_comments_data, db_session
+from src.logger import setup_logger
 
 logger = setup_logger("reddit_sentiment_tracker")
 
@@ -110,3 +111,86 @@ async def get_comments(subreddit_name: str, limit: int = 5) -> List[Dict[str, An
     except Exception as e:
         logger.error(f"Error retrieving Comments data of Subreddit '{subreddit_name}': {e}", exc_info=True)
         return []
+
+@app.post("/register")
+async def register(request: RegisterRequest) -> RegisterResponse:
+    """ Enduser can register using username, email, password """
+    try:
+        with db_session() as conn:
+            # checking if username already exists
+            existing_user = conn.execute(
+                users.select().where(users.c.username == request.username)
+            ).first()
+            if existing_user:
+                logger.warning(f"Registration failed: Username '{request.username}' already exists")
+                raise HTTPException(status_code=400, detail="Username already exists")
+
+            # checking if email already exists
+            existing_email = conn.execute(
+                users.select().where(users.c.email == request.email)
+            ).first()
+            if existing_email:
+                logger.warning(f"Registration failed: Email '{request.email}' already exists")
+                raise HTTPException(status_code=400, detail="Email already exists")
+
+            # hashing request.password with bcrypt
+            hashed_pw = hash_password(request.password)
+
+            # User Creation (if there are no duplicates) + DB insertion
+            conn.execute(users.insert().values(
+                username=request.username,
+                email=request.email,
+                hashed_password=hashed_pw
+            ))
+
+            logger.info(f"User '{request.username}' with email '{request.email}' successfully created.")
+
+            return RegisterResponse(status="success")
+
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.error(f"Registration failed: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/login", response_model=LoginResponse)
+async def login(request: LoginRequest):
+    """ Login Endpoint for enduser """
+    try:
+        with db_session() as conn:
+            # checking if username exists in Db
+            existing_user = conn.execute(
+                users.select().where(users.c.username == request.username)
+            ).first()
+
+            # throw Error if username does not exist
+            if not existing_user:
+                logger.warning(f"Login failed: Username '{request.username}' does not exist")
+                raise HTTPException(status_code=400, detail="Username does not exists")
+
+            # extract hashed_password from existing user result
+            hashed_password = existing_user.hashed_password
+
+            # verifying hashed_password
+            if verify_password(request.password, hashed_password):
+                logger.info("The password is correct")
+
+                # creating JWT Token with data
+                data = {"sub": existing_user.username, "user_id": existing_user.id}
+                jwt_token = create_access_token(data=data, expires_delta=None)
+
+                return LoginResponse(
+                    status="success",
+                    access_token=jwt_token,
+                    token_type="bearer"
+                )
+
+            else:
+                logger.warning("Invalid password for user: '{request.username}'. Login failed")
+                raise HTTPException(status_code=400, detail="Invalid password")
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.error(f"Login failed: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal server error during login")
