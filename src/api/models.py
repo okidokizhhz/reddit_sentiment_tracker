f58a4e071bf942151ca80763e614d81581909ae9
okidoki <akm1000@hotmail.de>
2025-10-22 17:25:09 +0200

    implemented request and response models for /register /login endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -1,1 +1,23 @@
+# ~/reddit_sentiment_tracker/src/api/models.py
+
 from pydantic import BaseModel, Field
+
+
+# /register
+class RegisterRequest(BaseModel):
+    username: str = Field(..., min_length=3, max_length=40)
+    email: str = Field(..., min_length=5, max_length=40)
+    password: str = Field(..., min_length=5, max_length=40)
+
+class RegisterResponse(BaseModel):
+    status: str
+
+# /login
+class LoginRequest(BaseModel):
+    username: str = Field(..., min_length=3, max_length=40)
+    password: str = Field(..., min_length=5, max_length=40)
+
+class LoginResponse(BaseModel):
+    status: str
+    access_token: str
+    token_type: str = "bearer"
