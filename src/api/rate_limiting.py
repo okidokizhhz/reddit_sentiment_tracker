12a77a4c734ca9a1d18c0668d655d5033bbfe69b
okidoki <akm1000@hotmail.de>
2025-10-29 12:04:24 +0100

    added async pattern, http exception, returns user_id

--- src/api/rate_limiting.py
+++ src/api/rate_limiting.py
@@ -2,33 +2,37 @@
 
 import logging
 import redis.asyncio as redis
+from fastapi import Depends, HTTPException
 from typing import Any
+from src.config import REDIS_URL, RATE_LIMIT_Redis, WINDOW_SIZE_Redis
+from .auth_dependencies import get_current_user
 
 logger = logging.getLogger("reddit_sentiment_tracker")
 
-def get_redis_client(REDIS_URL: str) -> Any:
+async def get_redis_client(REDIS_URL: str) -> Any:
     """ Getting Redis Client """
     try:
         redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+        await redis_client.ping()
 
         logger.info("Redis Client successfully retrieved")
+
         return redis_client
 
     except Exception as e:
         logger.error(f"Error initializing Redis Client: {e}", exc_info=True)
         raise
 
-async def rate_limit_check(
-    user_id: str,
-    redis_client: Any, 
-    RATE_LIMIT_Redis: int,
-    WINDOW_SIZE_Redis: int
-) -> bool:
+
+async def rate_limit_check(user_id: str = Depends(get_current_user)) -> str:
     """ Rate Limiting with redis. Check if user has exceeded the rate limit for api endpoints """
+
+    redis_client = await get_redis_client(REDIS_URL)   # get Redis Client
+
     try:
         key = f"rate_limit:{user_id}"
 
-        current = await redis_client.get(key)   # get current count - returns None if key doens't exist
+        current = await redis_client(key)   # get current count - returns None if key doens't exist
 
         if current is None:
             current_count = 0
@@ -37,7 +41,7 @@ async def rate_limit_check(
 
         if current_count >= RATE_LIMIT_Redis:         # check if limit is exceeded
             logger.warning(f"Rate limit exceeded for user {user_id}: {current_count}/{RATE_LIMIT_Redis}")
-            return False
+            raise HTTPException(status_code=429, detail=f"Rate limit exceeded for user {user_id}: {current_count}/{RATE_LIMIT_Redis}")
 
         async with redis_client.pipeline() as pipe:
             pipe.incr(key)
@@ -45,8 +49,13 @@ async def rate_limit_check(
             await pipe.execute()
 
         logger.info(f"Rate Limit check passed for user {user_id}: {current_count + 1}/{RATE_LIMIT_Redis}")
-        return True
 
+        return user_id
+
+    except HTTPException:
+        raise
     except Exception as e:
         logger.error(f"Rate Limit check failed: {e}", exc_info=True)
-        return True
+        return user_id
+    finally:
+        await redis_client.aclose()     # close redis connection
